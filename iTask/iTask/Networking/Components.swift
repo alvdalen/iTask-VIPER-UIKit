8784d665d69611c812782204ce524fdda293bf75
Adam Mirzakanov <adamirzakan@gmail.com>
2024-12-12 01:38:39 +0300

    feat: define the url components

---
+++ iTask/iTask/Networking/Components.swift
@@ -0,0 +1,19 @@
+//
+//  Components.swift
+//  iTask
+//
+//  Created by Адам Мирзаканов on 12.12.2024.
+//
+
+enum API: String {
+  case https = "https"
+  case host = "674d204454e1fca9290e726c.mockapi.io"
+}
+
+enum Endpoint: String {
+  case todos = "/todo"
+}
+
+enum HTTPMethod: String {
+  case get = "GET"
+}
